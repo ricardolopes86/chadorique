f4bff32a356b3078373be27a7512bc897982f562
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2018-08-19 23:57:17 +0200

    trying to fix passport

--- routes/auth.js
+++ routes/auth.js
@@ -14,10 +14,7 @@ module.exports = app => {
   
   app.get(
       "/auth/facebook",
-      passport.authenticate("facebook", {
-        scope: ['email'],
-        failureRedirect: '/entrar'
-        })
+      passport.authenticate("facebook")
     );
     
     app.get("/auth/facebook/callback", passport.authenticate("facebook"));
