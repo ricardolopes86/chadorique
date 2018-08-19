2133812b97085a17bb102a42eb89f28bf6016cea
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2018-08-20 00:03:26 +0200

    trying to fix passport

--- routes/auth.js
+++ routes/auth.js
@@ -5,23 +5,24 @@ module.exports = app => {
     "/auth/google",
     passport.authenticate("google", {
       scope: ["profile", "email"],
-      failureRedirect: '/entrar'
+      failureRedirect: "/entrar"
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
-
-  
   app.get(
-      "/auth/facebook",
-      passport.authenticate("facebook")
-    );
-    
-    app.get("/auth/facebook/callback", passport.authenticate("facebook"));
-    
-    app.get('/logout', (req, res) => {
-      req.logout();
-      res.send(req.user);
-    });
+    "/auth/google/callback",
+    passport.authenticate("google", {
+      successRedirect: "/",
+      failureRedirect: "/entrar"
+    })
+  );
 
+  app.get("/auth/facebook", passport.authenticate("facebook"));
+
+  app.get("/auth/facebook/callback", passport.authenticate("facebook"));
+
+  app.get("/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };
