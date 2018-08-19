6ca88e82ef1ac51645a4408d1c84a5b2da0428e0
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2018-08-19 23:04:23 +0200

    corrected passport login - removed facebook login

--- routes/auth.js
+++ routes/auth.js
@@ -11,14 +11,14 @@ module.exports = app => {
   app.get("/auth/google/callback", passport.authenticate("google"));
 
   
-  app.get(
-      "/auth/facebook",
-      passport.authenticate("facebook", {
-        scope: ['email']
-        })
-    );
+//   app.get(
+//       "/auth/facebook",
+//       passport.authenticate("facebook", {
+//         scope: ['email']
+//         })
+//     );
     
-    app.get("/auth/google/callback", passport.authenticate("facebook"));
+//     app.get("/auth/google/callback", passport.authenticate("facebook"));
     
     app.get('/logout', (req, res) => {
       req.logout();
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
+// const FacebookStrategy = require("passport-facebook").Strategy;
 const keys = require("../config/keys");
 
 const User = mongoose.model("users");
@@ -38,23 +38,23 @@ passport.use(
   )
 );
 
-passport.use(
-    new FacebookStrategy({
-    clientID: keys.facebookClientID,
-    clientSecret: keys.facebookClientSecret,
-    callbackURL: "/auth/facebook/callback"
-    //profileFields: ['id', 'displayName', 'email']
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOne({ facebookId: profile.id, name: profile.displayName }).then(existingUser => {
-        if (existingUser) {
-          //don't create user
-          done(null, existingUser);
-        } else {
-          new User({ facebookId: profile.id }).save().then(user => {
-            done(null, user);
-          });
-        }
-      });
-  }
-));
+// passport.use(
+//     new FacebookStrategy({
+//     clientID: keys.facebookClientID,
+//     clientSecret: keys.facebookClientSecret,
+//     callbackURL: "/auth/facebook/callback"
+//     //profileFields: ['id', 'displayName', 'email']
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOne({ facebookId: profile.id, name: profile.displayName }).then(existingUser => {
+//         if (existingUser) {
+//           //don't create user
+//           done(null, existingUser);
+//         } else {
+//           new User({ facebookId: profile.id }).save().then(user => {
+//             done(null, user);
+//           });
+//         }
+//       });
+//   }
+// ));
