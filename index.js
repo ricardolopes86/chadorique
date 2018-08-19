2e82f027837e465bc72dc29394572d895932e5c5
Ricardo da Silva <ricardolopes18@yahoo.com.br>
2018-08-20 00:16:20 +0200

    trying to fix passport

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
-require('./models/users');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/users");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/main')(app);
-require('./routes/auth')(app);
+app.use(function(req, res, next) {
+  res.locals.user = req.session.user;
+  next();
+});
+
+require("./routes/main")(app);
+require("./routes/auth")(app);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('Server running on port ' + PORT);
-})
+  console.log("Server running on port " + PORT);
+});
